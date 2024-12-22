c5d91cebe21d54b6a63607be80bd4e3d6ff46a7c
Rafael Costa <rafael.rcs.costa@gmail.com>
2024-12-22 12:21:25 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Screen Match Movies");
+        System.out.println("Filme: Jumanji");
+
+        int yearOfRelease = 1995;
+        System.out.println("Ano de lançamento: " + yearOfRelease);
+        boolean includedPlan = true;
+        double movieRating = 8.2;
+        }
+    }
