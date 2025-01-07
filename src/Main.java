ac47f4404feda7871f73aa4f9a32d05cf99e2119
Rafael Costa <rafael.rcs.costa@gmail.com>
2025-01-07 19:51:03 -0300

    Teste de conversão de valores

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Screen Match Movies");
         System.out.println("Filme: Jumanji");
+        
 
         int yearOfRelease = 1995;
         System.out.println("Ano de lançamento: " + yearOfRelease);
         boolean includedPlan = true;
         double movieRating = 8.2;
+        System.out.println(movieRating);
+        String sinopse;
+        sinopse = """
+                Filme Jumanji
+                Uma aventura do jogo de tabuleiro para a vida real!
+                Ano de lançamento
+                """ + yearOfRelease;
+        System.out.println(sinopse);
+
         }
     }
